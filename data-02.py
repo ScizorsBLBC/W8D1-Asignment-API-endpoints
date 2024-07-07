14f4bd90944681b419b519e79324c24dc9e89b9c
scizors <52053296+ScizorsBLBC@users.noreply.github.com>
2024-07-07 04:09:16 -0700

    Delete data-02.py

--- data-02.py
+++
@@ -1,6 +0,0 @@
-# complex calculation using descriptive names
-
-base_price = 100
-discount_percentage = 20
-final_price = base_price * (1-discount_percentage/100) 
-print("Discounted final price:", final_price)
